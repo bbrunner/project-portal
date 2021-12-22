7f1eb281b82a2732c087ef88f83f972e5a9bf099
kwwii <kwwii@protonmail.com>
2021-12-22 09:56:46 +0000

    Deploying to gh-pages from @ SUSE/project-portal@35178277bc104c4aee1ed65873feaf73e6f4e634 🚀

---
+++ _next/static/1xrlZmzfX_3WV9s6Oxknw/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-336b195712b8a3b0.js"],"/_error":["static/chunks/pages/_error-d742f979193aeae4.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB()
